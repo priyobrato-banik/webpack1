728897758c9e986982291fb7f46173fe9912765a
priyobratobanik <reach2priyo@gmail.com>
2022-04-29 01:24:57 +0530

    okay

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+function navbar(){
+    return `  
+    <h3>
+    <a href="/">Home</a>
+    </h3>
+    <div id="options">
+    <h3>
+    <a href="/jewelery.html">Jewelery</a>
+    </h3>
+    <h3>
+    <a href="/electronics.html">Electronics</a>
+    </h3>
+    <h3>
+    <a href="/login.html">Log In</a>
+    </h3>
+    <h3>
+    <a href="/signip.html">Sign Up</a>
+    </h3>
+    </div>
+  `;
+  
+}
+export{navbar}
